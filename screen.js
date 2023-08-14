90a7aac98f7ee5d45fc14b37d46ec8e2e0a73a6a
Keaden-Knight <knightkw@masters.edu>
2023-08-14 08:24:11 -0700

    did stuff

--- screen.js
+++ screen.js
@@ -3,21 +3,19 @@ import Camera from "./camera.js"
 export default class Screen {
     constructor () {
         this.background = document.createElement("canvas");
-        this.midground = document.querySelector("#main");
-        this.foreground = document.createElement("canvas");
+        this.midground = document.querySelector("#midground");
+        this.foreground = document.querySelector("#foreground");
         this.bgctx = this.background.getContext("2d");
         this.mgctx = this.midground.getContext("2d");
         this.fgctx = this.foreground.getContext("2d");
 
-        this.mgctx.beginPath()
-        this.mgctx.moveTo(0, 0)
-        this.mgctx.lineTo(100, 100)
-        this.mgctx.stroke()
+        
         //image location is temporary
         let img = new Image();
-        img.onload = function () {
-            this.mgctx.drawImage(img, 0, 0);
+        img.onload = () => {
+            this.bgctx.drawImage(img, 0, 0);
+            this.mgctx.drawImage(this.background, 50, 50, 50, 50, 0, 0, this.midground.clientWidth, this.midground.clientHeight);
         };
-        img.src = "./test_background.svg"
+        img.src = "./test_background.svg";
     }
 }
