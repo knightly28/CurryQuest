4f0da8a3268e60c5d80155266cf38cbf1c8ec2ff
Keaden-Knight <knightkw@masters.edu>
2023-08-13 17:40:26 -0700

    added stuff

--- game.js
+++ game.js
@@ -1,27 +1,52 @@
-
+import gameEvents from "./gameEvents.json" assert {type: 'json'};
 
 export default class Game {
     constructor () {
-        this.events = []
-        //this.availEvnts = []
+        this.flags = 0
+        this.allEvents = gameEvents
+        this.activeEvents = []
         this.bypassKeys = ["p", "m"]
     }
 
-    addEvent (priority, flag) {
+    /*addEvent (priority, flag) {
         this.events.splice(this.events.findIndex(v => v.priority > priority), 0, {priority: priority, flag: flag});
-    }
+    }*/
 
     resolveEvents () {
-        for (let e of this.events) {
-
+        for (let e of this.activeEvents) {
+            
         }
     }
 
+    flagEvent (flags) {
+        for (let flag of flags) {
+            if (flag[1] === "&") {
+                this.flags &= ~(2 ** flag[0])
+            } else if (flag[1] === "|") {
+                this.flags |= (2 ** flag[0])
+            } else if (flag[1] === "^") {
+                this.flags ^= (2 ** flag[0])
+            }
+        }
+    }
+
+    readEvent (lines) {
+        return new Promise((resolve) => {
+            (async () => {
+                for await (let line of lines) {
+                    //put text on a banner
+                    console.log(line)
+                    await this.awaitKeyPress(" ")
+                }
+            })()
+            resolve()
+        });
+    }
+
     awaitKeyPress (...only) {
-        let temp = this.bypassKeys;
         return new Promise((resolve) => {
             document.addEventListener('keydown', (e) => {
-                if (only.includes(e.key) || !(only.length || temp.includes(e.key))) {
+                if (only.includes(e.key) || !(only.length || this.bypassKeys.includes(e.key))) {
                     resolve();
                 }
             });
--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-//import Game from "https://github.com/knightly28/CurryQuest/blob/main/game.js"
+import Game from "./game.js"
 
 const tempGame = new Game();
-(async () => {
+/*(async () => {
     console.log("press the 'a' key")
-    await tempGame.awaitKeyPress();
+    await tempGame.awaitKeyPress(" ");
     console.log("you pressed the 'a' key");
-})()
+})()*/
+
+tempGame.readEvent(["this is a test", "this is also a test", "this is a third test"])
